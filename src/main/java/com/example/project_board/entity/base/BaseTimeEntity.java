f32f4724f9fdc7620baf7e0c7160774f88749341
YERIMOH <koki57176550@gmail.com>
2022-08-18 10:42:54 +0900

    게시판 만들기(게시글 등록&수정 / 회원관리) check

---
+++ src/main/java/com/example/project_board/entity/base/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.example.project_board.entity.base;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@Setter
+@Getter
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private Date createDate;
+
+    @LastModifiedDate
+    private Date updateDate;
+}
